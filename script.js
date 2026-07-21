4b22c955e28a4bf0b17e1cf4880b1766fe6db748
gabrielbrandaoborbafeitosa-ops <gabrielbrandaoborbafeitosa@gmail.com>
2026-07-21 14:37:18 -0300

    novo JS
    Build 2

--- script.js
+++ script.js
@@ -23,9 +23,36 @@ function maisextras(extras, pt) {
         alert("Não há pontos suficientes.");
     }
 }
-/*
 function converter() {
-    localStorage.setItem('pontos', pontos + extrasvar);
+localStorage.setItem(
+    "pontos",
+    Number(localStorage.getItem("pontos") || 0) +
+    Number(localStorage.getItem("extras") || 0)
+);
+
+    const botao = document.getElementById("converter");
+
+const fimEspera = localStorage.getItem("fimEspera");
+
+if (fimEspera && Date.now() < Number(fimEspera)) {
+    botao.disabled = true;
+
+    setTimeout(() => {
+        botao.disabled = false;
+        localStorage.removeItem("fimEspera");
+    }, Number(fimEspera) - Date.now());
+
+} else {
+    botao.disabled = false;
+
+    const umaSemana = 7 * 24 * 60 * 60 * 1000;
+    localStorage.setItem("fimEspera", Date.now() + umaSemana);
+
+    botao.disabled = true;
+
+    setTimeout(() => {
+        botao.disabled = false;
+        localStorage.removeItem("fimEspera");
+    }, umaSemana);
 }
-    COMING SOON
-    */
+}
